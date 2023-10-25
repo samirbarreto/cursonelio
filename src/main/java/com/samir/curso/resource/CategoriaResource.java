14b54f36fce9e8805dff8fdedcd517a413f77466
Samir Barreto <samir.barreto@ssp.ba.gov.br>
2023-10-25 14:46:31 -0300

    teste commit

--- src/main/java/com/samir/curso/resource/CategoriaResource.java
+++ src/main/java/com/samir/curso/resource/CategoriaResource.java
@@ -8,10 +8,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping (value="/categorias")
 public class CategoriaResource {
 
-	
 	@RequestMapping(method=RequestMethod.GET)
 	public String listar() {
-		return "REST está funcionando";
+		return ("REST está funcionando");
 
 	}
 
