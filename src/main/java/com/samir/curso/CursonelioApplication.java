98433c491b382c4bea81ba00a8f5b790e90619f1
Samir Barreto <samir.barreto@ssp.ba.gov.br>
2023-10-25 13:58:55 -0300

    segundo commit

--- src/main/java/com/samir/curso/CursonelioApplication.java
+++ src/main/java/com/samir/curso/CursonelioApplication.java
@@ -3,11 +3,14 @@ package com.samir.curso;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import ch.qos.logback.core.net.SyslogOutputStream;
+
 @SpringBootApplication
 public class CursonelioApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursonelioApplication.class, args);
+	System.out.println("hellou");
 	}
-
+      
 }
