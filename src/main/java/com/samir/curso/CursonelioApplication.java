befa69cbec8af31dbce50e5b74e3891d8b0182cf
Samir <samir.barreto@ssp.ba.gov.br>
2023-10-25 13:41:17 -0300

    inital commit

---
+++ src/main/java/com/samir/curso/CursonelioApplication.java
@@ -0,0 +1,13 @@
+package com.samir.curso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursonelioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursonelioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/samir/curso/CursonelioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samir.curso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursonelioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
