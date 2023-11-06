cb5e50ffa63275a61e8b24a08550f48d3dacc435
863401675 <863401675@host.docker.internal>
2023-11-06 14:41:51 -0300

    produto e associacao muitos para muitos

--- src/main/java/com/samir/curso/CursonelioApplication.java
+++ src/main/java/com/samir/curso/CursonelioApplication.java
@@ -8,15 +8,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.samir.curso.domain.Categoria;
+import com.samir.curso.domain.Produto;
 import com.samir.curso.repositories.CategoriaRepository;
+import com.samir.curso.repositories.ProdutoRepository;
 
 @SpringBootApplication
-public class CursonelioApplication implements CommandLineRunner{
+public class CursonelioApplication implements CommandLineRunner {
 	@Autowired
 	private CategoriaRepository categoriaRepository;
- 	
-	
-	
+	@Autowired
+	private ProdutoRepository produtoRepository;
 	public static void main(String[] args) {
 		SpringApplication.run(CursonelioApplication.class, args);
 	}
@@ -26,13 +27,22 @@ public class CursonelioApplication implements CommandLineRunner{
 
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
-		
+
+		Produto p1 = new Produto(null, "computador", 20000.00);
+
+		Produto p2 = new Produto(null, "impressora", 20000.00);
+
+		Produto p3 = new Produto(null, "mouse", 800.00);
+
+		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
-		
-		
-		
-		
-	}
-	
-      
+		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+		}
+
 }
