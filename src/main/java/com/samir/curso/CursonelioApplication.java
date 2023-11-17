865178aa09f1d417da24c6797f595108552c1f7e
863401675 <863401675@host.docker.internal>
2023-11-17 16:31:33 -0300

    pagamento

--- src/main/java/com/samir/curso/CursonelioApplication.java
+++ src/main/java/com/samir/curso/CursonelioApplication.java
@@ -25,6 +25,8 @@ import com.samir.curso.repositories.CidadeRepository;
 import com.samir.curso.repositories.ClienteRepository;
 import com.samir.curso.repositories.EnderecoRepository;
 import com.samir.curso.repositories.EstadoRepository;
+import com.samir.curso.repositories.PagamentoRepository;
+import com.samir.curso.repositories.PedidoRepository;
 import com.samir.curso.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -41,7 +43,12 @@ public class CursonelioApplication implements CommandLineRunner {
 	private ClienteRepository clienteRepository;
 	@Autowired
 	private EnderecoRepository enderecoRepository;
-
+	@Autowired
+	private PedidoRepository pedidoRepository;
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
+	
+		
 	public static void main(String[] args) {
 		SpringApplication.run(CursonelioApplication.class, args);
 	}
@@ -99,12 +106,14 @@ public class CursonelioApplication implements CommandLineRunner {
 		Pagamento pagto1 = new PagamentoComCartao(null, EstadoPagamento.QUITADO, ped1, 6);
 		ped1.setPagamento(pagto1);
 
-		Pagamento pagto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, sdf.parse("20/10/2022 00:00"),
-				null);
+		Pagamento pagto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, sdf.parse("20/10/2022 00:00"),null);
 		ped1.setPagamento(pagto2);
 
 		cli1.getPedidos().addAll(Arrays.asList(ped1, ped2));
+		
+		pedidoRepository.saveAll(Arrays.asList(ped1,ped2));
+		pagamentoRepository.saveAll(Arrays.asList(pagto1,pagto2));
 
-	}
+	} 
 
 }
