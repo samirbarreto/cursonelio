0799ec917b3f658943bca4254ced35ebc13a1e3b
863401675 <863401675@host.docker.internal>
2023-11-22 15:54:55 -0300

    find page

--- src/main/java/com/samir/curso/CursonelioApplication.java
+++ src/main/java/com/samir/curso/CursonelioApplication.java
@@ -62,7 +62,13 @@ public class CursonelioApplication implements CommandLineRunner {
 
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
+		Categoria cat3 = new Categoria(null, "Cama mesa e  banho");
+		Categoria cat4 = new Categoria(null, "Eletrônicos");
+		Categoria cat5 = new Categoria(null, "jardinagem");
+		Categoria cat6 = new Categoria(null, "decoração");
+		Categoria cat7 = new Categoria(null, "perfumaria");
 
+		
 		Produto p1 = new Produto(null, "computador", 20000.00);
 
 		Produto p2 = new Produto(null, "impressora", 20000.00);
@@ -76,7 +82,7 @@ public class CursonelioApplication implements CommandLineRunner {
 		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
 
-		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2,cat3,cat4,cat5,cat6,cat7));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 		
 		Estado est1 = new Estado(null, "minas Gerais");
--- src/main/java/com/samir/curso/services/CategoriaService.java
+++ src/main/java/com/samir/curso/services/CategoriaService.java
@@ -1,9 +1,13 @@
 package com.samir.curso.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.samir.curso.domain.Categoria;
@@ -43,4 +47,14 @@ public class CategoriaService {
 		}
 		repo.deleteById(id);
 	}
+
+	public List<Categoria> findAll() {
+		return repo.findAll();
+		}
+
+	public Page<Categoria> findPage(Integer page,Integer linesPerPage, String orderby,String direction){
+		PageRequest pageRequest =  PageRequest.of(page, linesPerPage, Direction.valueOf(direction),orderby); 
+		return repo.findAll(pageRequest);	
+	}
+
 }
