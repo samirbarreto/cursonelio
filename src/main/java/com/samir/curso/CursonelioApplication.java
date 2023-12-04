a429437432033ceed4e29a3ba676e38fe53664ba
863401675 <863401675@host.docker.internal>
2023-12-04 17:06:19 -0300

    busca de pedidos

--- src/main/java/com/samir/curso/CursonelioApplication.java
+++ src/main/java/com/samir/curso/CursonelioApplication.java
@@ -52,7 +52,7 @@ public class CursonelioApplication implements CommandLineRunner {
 	private PagamentoRepository pagamentoRepository;
 	@Autowired
 	private ItemPedidoRepository itemPedidoRepository;
-	
+
 	public static void main(String[] args) {
 		SpringApplication.run(CursonelioApplication.class, args);
 	}
@@ -68,23 +68,41 @@ public class CursonelioApplication implements CommandLineRunner {
 		Categoria cat6 = new Categoria(null, "decoração");
 		Categoria cat7 = new Categoria(null, "perfumaria");
 
-		
-		Produto p1 = new Produto(null, "computador", 20000.00);
-
-		Produto p2 = new Produto(null, "impressora", 20000.00);
-
+		Produto p1 = new Produto(null, "computador", 2000.00);
+		Produto p2 = new Produto(null, "impressora", 500.00);
 		Produto p3 = new Produto(null, "mouse", 800.00);
+		Produto p4 = new Produto(null, "Mesa de escritório", 300.00);
+		Produto p5 = new Produto(null, "Toalha", 50.00);
+		Produto p6 = new Produto(null, "Colcha", 200.00);
+		Produto p7 = new Produto(null, "Tv 2k", 1200.00);
+		Produto p8 = new Produto(null, "Roçadeira", 750.00);
+		Produto p9 = new Produto(null, "Abajour", 100.00);
+		Produto p10 = new Produto(null, "Pendente", 180.00);
+		Produto p11 = new Produto(null, "Shampoo", 90.00);
 
 		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
-		cat2.getProdutos().addAll(Arrays.asList(p2));
+		cat2.getProdutos().addAll(Arrays.asList(p2, p4));
+		cat3.getProdutos().addAll(Arrays.asList(p5, p6));
+		cat4.getProdutos().addAll(Arrays.asList(p1, p2, p3, p7));
+		cat5.getProdutos().addAll(Arrays.asList(p8));
+		cat6.getProdutos().addAll(Arrays.asList(p9, p10));
+		cat7.getProdutos().addAll(Arrays.asList(p11));
 
-		p1.getCategorias().addAll(Arrays.asList(cat1));
-		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
-		p3.getCategorias().addAll(Arrays.asList(cat1));
+		p1.getCategorias().addAll(Arrays.asList(cat1, cat4));
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2, cat4));
+		p3.getCategorias().addAll(Arrays.asList(cat1, cat4));
+		p4.getCategorias().addAll(Arrays.asList(cat2));
+		p5.getCategorias().addAll(Arrays.asList(cat3));
+		p6.getCategorias().addAll(Arrays.asList(cat3));
+		p7.getCategorias().addAll(Arrays.asList(cat4));
+		p8.getCategorias().addAll(Arrays.asList(cat5));
+		p9.getCategorias().addAll(Arrays.asList(cat6));
+		p10.getCategorias().addAll(Arrays.asList(cat6));
+		p11.getCategorias().addAll(Arrays.asList(cat7));
 
-		categoriaRepository.saveAll(Arrays.asList(cat1, cat2,cat3,cat4,cat5,cat6,cat7));
-		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
-		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2, cat3, cat4, cat5, cat6, cat7));
+		produtoRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11));
+
 		Estado est1 = new Estado(null, "minas Gerais");
 		Estado est2 = new Estado(null, "São paulo");
 
@@ -125,20 +143,20 @@ public class CursonelioApplication implements CommandLineRunner {
 
 		pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
 		pagamentoRepository.saveAll(Arrays.asList(pagto1, pagto2));
-        
+
 		ItemPedido ip1 = new ItemPedido(ped1, p1, 0.00, 1, 2000.00);
 		ItemPedido ip2 = new ItemPedido(ped1, p3, 0.00, 2, 80.00);
-		ItemPedido ip3 = new ItemPedido(ped2,p2,100.00,1,800.00);
-		
-		ped1.getItens().addAll(Arrays.asList(ip1,ip2));
+		ItemPedido ip3 = new ItemPedido(ped2, p2, 100.00, 1, 800.00);
+
+		ped1.getItens().addAll(Arrays.asList(ip1, ip2));
 		ped2.getItens().addAll(Arrays.asList(ip3));
-		
+
 		p1.getItens().addAll(Arrays.asList(ip1));
 		p2.getItens().addAll(Arrays.asList(ip3));
 		p3.getItens().addAll(Arrays.asList(ip2));
-		
-		itemPedidoRepository.saveAll(Arrays.asList(ip1,ip2,ip3));
-		
+
+		itemPedidoRepository.saveAll(Arrays.asList(ip1, ip2, ip3));
+
 	}
 
 }
--- src/main/java/com/samir/curso/services/ProdutoService.java
+++ src/main/java/com/samir/curso/services/ProdutoService.java
@@ -1,12 +1,17 @@
 package com.samir.curso.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.samir.curso.domain.Categoria;
 import com.samir.curso.domain.Produto;
+import com.samir.curso.repositories.CategoriaRepository;
 import com.samir.curso.repositories.ProdutoRepository;
 import com.samir.curso.services.exception.ObjectNotFoundException;
 
@@ -14,12 +19,20 @@ import com.samir.curso.services.exception.ObjectNotFoundException;
 public class ProdutoService {
 	@Autowired
 	private ProdutoRepository repo;
-
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 	public Produto find(Integer id) {
 		Optional<Produto> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", tipo: " + Categoria.class.getName()));
-
+		
+		
 	}
-
+	
+	public Page<Produto> search(String nome , List<Integer> ids,Integer page,Integer linesPerPage, String orderby,String direction){
+		PageRequest pageRequest =  PageRequest.of(page, linesPerPage, Direction.valueOf(direction),orderby);  
+		List<Categoria> categorias =  categoriaRepository.findAllById(ids);
+		return repo.findDistrinctByNomeContainingAndCategoriasIn(nome,categorias,pageRequest);
+	
+	}
 }
