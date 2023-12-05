c3b1354f8329ab389c555f5158ee1770f3f00aed
863401675 <863401675@host.docker.internal>
2023-12-05 17:01:07 -0300

    pedido service

---
+++ src/main/java/com/samir/curso/services/BoletoService.java
@@ -0,0 +1,26 @@
+package com.samir.curso.services;
+
+import java.util.Calendar;
+import java.util.Date;
+
+import org.springframework.stereotype.Service;
+
+import com.samir.curso.domain.PagamentoComBoleto;
+
+@Service
+public class BoletoService {
+ public void preencherPagamentoComBoleto(PagamentoComBoleto pagto,Date instanteDoPedido) {
+	 Calendar cal = Calendar.getInstance();
+	 cal.setTime(instanteDoPedido);
+	 cal.add(Calendar.DAY_OF_MONTH,7);
+	 pagto.setDataVencimento(cal.getTime());
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+}
+ }
