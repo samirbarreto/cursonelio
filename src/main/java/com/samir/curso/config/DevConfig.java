21c1e65a86ca7fe09491fba562006f7a84645767
863401675 <863401675@host.docker.internal>
2023-12-06 16:59:20 -0300

    banco

---
+++ src/main/java/com/samir/curso/config/DevConfig.java
@@ -0,0 +1,26 @@
+package com.samir.curso.config;
+
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.samir.curso.services.DBservice;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+
+	
+	@Autowired
+	private DBservice dbService;
+	
+	@Bean
+	public boolean instantiateDatabase() throws ParseException	{
+		dbService.instantiateTestDatabase();
+		return true;
+	}
+	
+}
