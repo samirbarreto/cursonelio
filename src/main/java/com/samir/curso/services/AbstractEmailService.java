8b575af052a01a617a30de1bb2076b6a750ffa27
863401675 <863401675@host.docker.internal>
2023-12-22 15:54:20 -0300

    Esqueci a senha

--- src/main/java/com/samir/curso/services/AbstractEmailService.java
+++ src/main/java/com/samir/curso/services/AbstractEmailService.java
@@ -4,6 +4,7 @@ import java.util.Date;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 
+import com.samir.curso.domain.Cliente;
 import com.samir.curso.domain.Pedido;
 
 public abstract class AbstractEmailService implements EmailService {
@@ -26,4 +27,19 @@ public abstract class AbstractEmailService implements EmailService {
 		sm.setText(obj.toString());
 		return sm;
 	}
+	public void sendNewPasswordEmail(Cliente cliente, String newPass) {
+		SimpleMailMessage sm = prepareNewPasswordEmail(cliente, newPass);
+		sendEmail(sm);
+	}
+
+	protected SimpleMailMessage prepareNewPasswordEmail(Cliente cliente, String newPass) {
+		SimpleMailMessage sm = new SimpleMailMessage();
+		sm.setTo(cliente.getEmail());
+		sm.setFrom(sender);
+		sm.setSubject("Solicitação de nova senha");
+		sm.setSentDate(new Date(System.currentTimeMillis()));
+		sm.setText("Nova senha: " + newPass);
+		return sm;
+	}
+	
 }
--- src/main/java/com/samir/curso/services/EmailService.java
+++ src/main/java/com/samir/curso/services/EmailService.java
@@ -4,6 +4,7 @@ package com.samir.curso.services;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.stereotype.Service;
 
+import com.samir.curso.domain.Cliente;
 import com.samir.curso.domain.Pedido;
 
 @Service
@@ -12,4 +13,6 @@ public interface EmailService {
 	void sendOrderConfirmationEmail(Pedido obj);
 
 	void sendEmail(SimpleMailMessage msg);
+	
+	void sendNewPasswordEmail(Cliente cliente, String newPass);
 }
